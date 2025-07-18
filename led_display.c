0f59b46db239c56ad63e0b40c8caab23e4c9e0d4
SyedDevop <syeds.devops007@gmail.com>
2025-07-18 23:59:43 +0530

    feat: add letter and number

--- led_display.c
+++ led_display.c
@@ -18,6 +18,7 @@
 #define PANEL_COUNT 1
 #define NUM_PIXEL ((GRID_WIDTH * GRID_HEIGHT) * PANEL_COUNT)
 #define LED_PIN 2
+#define num_to_char(num) (num + 48)
 
 static inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb) {
   pio_sm_put_blocking(pio, sm, pixel_grb << 8);
@@ -57,10 +58,136 @@ static inline void show_pixel(PIO pio, uint sm) {
     put_pixel(pio, sm, pixels[i]);
   }
 }
+#define DIGIT_FONT_WIDTH 4
+#define DIGIT_FONT_HEIGHT 6
+
+#define LETTER_FONT_WIDTH 5
+#define LETTER_FONT_HEIGHT 5
+
+typedef bool DIGIT_FONT[DIGIT_FONT_WIDTH * DIGIT_FONT_HEIGHT];
+typedef bool LETTER_FONT[LETTER_FONT_WIDTH * LETTER_FONT_HEIGHT];
+
+static const DIGIT_FONT DIGIT_FONTS[10] = {
+    // 0
+    {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0},
+    // 1
+    {0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1},
+    // 2
+    {0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1},
+    // 3
+    {1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0},
+    // 4
+    {0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0},
+    // 5
+    {1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0},
+    // 6
+    {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0},
+    // 7
+    {1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0},
+    // 8
+    {0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0},
+    // 9
+    {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0},
+};
+
+static const LETTER_FONT LETTER_FONTS[26] = {
+    // a
+    {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+    // b
+    {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0},
+    // c
+    {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0},
+    // d
+    {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0},
+    // e
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+    // f
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    // g
+    {0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0},
+    // h
+    {1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+    // i
+    {1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1},
+    // j
+    {0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0},
+    // k
+    {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0},
+    // l
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+    // m
+    {1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+    // n
+    {1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1},
+    // o
+    {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0},
+    // p
+    {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    // q
+    {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1},
+    // r
+    {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+    // s
+    {0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0},
+    // t
+    {1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+    // u
+    {1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0},
+    // v
+    {1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
+    // w
+    {1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1},
+    // x
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1},
+    // y
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+    // z
+    {1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1},
+};
+
+static inline void put_digit_in_pixel_buff(const DIGIT_FONT font, int base_x,
+                                           int base_y, uint32_t color) {
+  for (int row = 0; row < DIGIT_FONT_HEIGHT; ++row) {
+    for (int col = 0; col < DIGIT_FONT_WIDTH; ++col) {
+      int font_index = row * DIGIT_FONT_WIDTH + col;
+      if (font[font_index]) {
+        int pixel_index = (base_y + row) * GRID_WIDTH + (base_x + col);
+        if (pixel_index >= 0 && pixel_index < NUM_PIXEL) {
+          pixels[pixel_index] = color;
+        }
+      }
+    }
+  }
+}
+
+static inline void put_letter_in_pixel_buff(const LETTER_FONT font, int base_x,
+                                            int base_y, uint32_t color) {
+  for (int row = 0; row < LETTER_FONT_HEIGHT; ++row) {
+    for (int col = 0; col < LETTER_FONT_WIDTH; ++col) {
+      int font_index = row * LETTER_FONT_WIDTH + col;
+      if (font[font_index]) {
+        int pixel_index = (base_y + row) * GRID_WIDTH + (base_x + col);
+        if (pixel_index >= 0 && pixel_index < NUM_PIXEL) {
+          pixels[pixel_index] = color;
+        }
+      }
+    }
+  }
+}
+
+static inline void put_char_in_pixel_buff(char c, int base_x, int base_y,
+                                          uint32_t color) {
+  if (c >= '0' && c <= '9') {
+    put_digit_in_pixel_buff(DIGIT_FONTS[c - '0'], base_x, base_y, color);
+  } else if (c >= 'A' && c <= 'Z') {
+    put_letter_in_pixel_buff(LETTER_FONTS[c - 'A'], base_x, base_y, color);
+  } else if (c >= 'a' && c <= 'z') {
+    put_letter_in_pixel_buff(LETTER_FONTS[c - 'a'], base_x, base_y, color);
+  }
+}
 
 int main() {
   stdio_init_all();
-  sleep_ms(1000);
   printf("Starting, ws2812 with %d LEDs using pin %d\n", NUM_PIXEL, LED_PIN);
 
   // // Initialise the Wi-Fi chip
@@ -81,21 +208,30 @@ int main() {
   ws2812_program_init(pio, sm, offset, LED_PIN, 800000, false);
 
   while (true) {
-    for (int i = 0; i < NUM_PIXEL; i++) {
-      put_pixel_buff(i, i % 2 == 0 ? rgba_u32(255, 0, 0, 1)
-                                   : rgba_u32(0, 255, 0, 1));
+    for (int i = 60; i > 0; i--) {
+
+      put_char_in_pixel_buff(num_to_char(i / 10), 0, 0,
+                             rgba_u32(50, 100, 150, 10));
+      put_char_in_pixel_buff(num_to_char(i % 10), 4, 1,
+                             rgba_u32(50, 100, 150, 10));
 
       show_pixel(pio, sm);
-      sleep_ms(100);
+      sleep_ms(1000);
+      clear_buff();
     }
-    for (int i = NUM_PIXEL; i > 0; i--) {
-      put_pixel_buff(i, 0);
+
+    for (int i = 0; i < 26; i++) {
+      put_char_in_pixel_buff(i + 'a', 0, 0, rgba_u32(50, 100, 150, 10));
       show_pixel(pio, sm);
-      sleep_ms(100);
+      sleep_ms(1000);
+      clear_buff();
     }
-    clear_buff();
-    show_pixel(pio, sm);
-    sleep_ms(500);
+
+    // put_letter_in_pixel_buff(LETTER_FONTS['l' - 97], 0, 0,
+    //                          rgba_u32(50, 100, 150, 10));
+    // show_pixel(pio, sm);
+    // sleep_ms(500);
+    // clear_buff();
   }
 
   pio_remove_program_and_unclaim_sm(&ws2812_program, pio, sm, offset);
